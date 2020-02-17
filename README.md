cc790f11e722d4fb7dd661f64e957f6bbf8a0917
Marius Bora <marborav@gmail.com>
2020-02-17 17:01:30 +0100

    power set with tree method

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 This repository contains examples to help me understand the mathematical concepts in the book ["Communicating Sequential Processes" by C.A.R Hoare](http://www.usingcsp.com/cspbook.pdf).
 
-I will try to represent in code the various mathematical formulaes expressed in this book. 
+I will try to represent in code the various mathematical formulae expressed in this book. 
+
+
+#### Done
+
+- logic
+- sets
--- sets/main.go
+++ sets/main.go
@@ -9,7 +9,7 @@ func IsAMemberOf(m string) {
 	A := []string{"a", "b", "c"}
 	for _, x := range A {
 		if x == m {
-			fmt.Printf("\n∈, %v is a member of %v, result %v", m,A,true)
+			fmt.Printf("\n∈, %v is a member of %v, result %v", m, A, true)
 			return
 		}
 	}
@@ -22,7 +22,7 @@ func IsNotAMemberOf(m string) {
 	A := []string{"a", "b", "c"}
 	for _, x := range A {
 		if x != m {
-			fmt.Printf("\n∉, %v is not a member of %v, result %v", m,A,true)
+			fmt.Printf("\n∉, %v is not a member of %v, result %v", m, A, true)
 			return
 		}
 	}
@@ -31,16 +31,16 @@ func IsNotAMemberOf(m string) {
 // Notation: {x|P(x)}
 // Meaning:  the set of all x such that P(x)
 // Example:  {E}={x|x % 2 = 0}
-func TheSetOfAllxSuchThatP(n []int)  {
+func TheSetOfAllxSuchThatP(n []int) {
 	var N []int
 	if len(n) > 0 {
 		N = n
 	} else {
-		N = []int{1,2,3,4,5,6,7,8,9}
+		N = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	}
 	var E []int
 	for _, x := range N {
-		if x % 2 == 0 {
+		if x%2 == 0 {
 			E = append(E, x)
 		}
 	}
@@ -51,41 +51,41 @@ func TheSetOfAllxSuchThatP(n []int)  {
 // Meaning:  A union B
 // Example:  A∪B={x|x∈A ∨ x∈B}
 func AUnionB() {
-	var A,B,C uint8
+	var A, B, C uint8
 	A = 2
 	B = 4
 	C = A | B // 6
-	fmt.Printf("\nA∪B, A∪B={x|x∈A ∨ x∈B}, A=%08b, B=%08b, result = %08b", A,B,C)
+	fmt.Printf("\nA∪B, A∪B={x|x∈A ∨ x∈B}, A=%08b, B=%08b, result = %08b", A, B, C)
 }
 
 // Notation: A∩B
 // Meaning:  A intersect B
 // Example:  A∩B={x|x∈A ∧ x∈B}
-func AIntersectB()  {
-	var A,B,C uint8
+func AIntersectB() {
+	var A, B, C uint8
 	A = 9
 	B = 5
 	C = A & B
-	fmt.Printf("\nA∩B, A∪B={x|x∈A ∨ x∈B}, A=%08b, B=%08b, result = %08b", A,B,C)
+	fmt.Printf("\nA∩B, A∪B={x|x∈A ∨ x∈B}, A=%08b, B=%08b, result = %08b", A, B, C)
 }
 
 // Notation: A−B
 // Meaning:  A minus B
 // Example:  A−B={x|x∈A ∧ ¬x∈B}
-func AMinusB()  {
-	var A,B,C uint8
+func AMinusB() {
+	var A, B, C uint8
 	A = 9
 	B = 5
 	C = A & (A ^ B)
 	//C = A ^ B // 6
-	fmt.Printf("\nA-B, A−B={x|x∈A ∧ ¬x∈B}, A=%08b, B=%08b, result = %08b", A,B,C)
+	fmt.Printf("\nA-B, A−B={x|x∈A ∧ ¬x∈B}, A=%08b, B=%08b, result = %08b", A, B, C)
 }
 
 // Notation: A ⊆ B
 // Meaning:  A is contained in B ( A is a subset of B)
 // Example:  A ⊆ B ≡ ∀x:A•x∈B
 func AIsContainedInB(a, b uint8) uint8 {
-	var A,B,C uint8
+	var A, B, C uint8
 	var isSubset bool
 	A = a
 	B = b
@@ -96,15 +96,15 @@ func AIsContainedInB(a, b uint8) uint8 {
 	} else {
 		isSubset = false
 	}
-	fmt.Printf("\nA ⊆ B, A ⊆ B ≡ ∀x:A•x∈B, A=%08b, B=%08b, result = %08b %v", A,B,C, isSubset)
+	fmt.Printf("\nA ⊆ B, A ⊆ B ≡ ∀x:A•x∈B, A=%08b, B=%08b, result = %08b %v", A, B, C, isSubset)
 	return C
 }
 
 // Notation: A ⊇ B
 // Meaning:  A contains B
 // Example:  A⊇B ≡ B⊆A
-func AContainsB()  {
-	var A,B,C uint8
+func AContainsB() {
+	var A, B, C uint8
 	var contains bool
 	A = 255
 	B = 5
@@ -115,16 +115,78 @@ func AContainsB()  {
 	} else {
 		contains = false
 	}
-	fmt.Printf("\nA ⊇ B, A⊇B ≡ B⊆A, A=%08b, B=%08b, result = %08b %v", A,B,C, contains)
+	fmt.Printf("\nA ⊇ B, A⊇B ≡ B⊆A, A=%08b, B=%08b, result = %08b %v", A, B, C, contains)
 }
 
 // Notation: {x:A|P(x)}
 // Meaning    the set of x in A such that P(x)
-func TheSetOfxInASuchThatP()  {
-	TheSetOfAllxSuchThatP([]int{2,2,2,4,4,5,5,6,7,7,7,8,8})
+func TheSetOfxInASuchThatP() {
+	TheSetOfAllxSuchThatP([]int{2, 2, 2, 4, 4, 5, 5, 6, 7, 7, 7, 8, 8})
 }
 
-func main()  {
+type tree struct {
+	set         []int
+	left, right *tree
+}
+
+// P (A)
+// |A|
+// for each element in the set, you can either add it to a subset, or not add it
+//
+func ThePowerSetOfA() {
+	//var powerSet
+	A := []int{-1, 3}
+
+	t := &tree{
+		set: []int{},
+	}
+
+	for _, el := range A {
+		t = expand(el, t)
+		//t = dontAdd(el, t)
+	}
+	var i int
+	i = 0
+	//powerSet := make(map[int][]int)
+	powerSet := make(map[int][]int)
+
+	powerSet = groupSets(t, powerSet, &i)
+
+	// for each set if not already duplicated
+
+	fmt.Println(powerSet)
+}
+
+func groupSets(t *tree, r map[int][]int, i *int) map[int][]int {
+	if t != nil {
+		*i++
+		r[*i] = t.set
+		// some sets are repeated as they are the previous step in the tree, I should filter duplicates out for printing sake
+		groupSets(t.left, r, i)
+		groupSets(t.right, r, i)
+	}
+	return r
+}
+
+func expand(el int, t *tree) *tree {
+	if t.left == nil {
+		t.left = &tree{
+			set: append(t.set, el),
+		}
+	} else {
+		expand(el, t.left)
+	}
+	if t.right == nil {
+		t.right = &tree{
+			set: t.set,
+		}
+	} else {
+		expand(el, t.right)
+	}
+	return t
+}
+
+func main() {
 	IsAMemberOf("a")
 	IsNotAMemberOf("ç")
 	TheSetOfAllxSuchThatP([]int{})
@@ -135,4 +197,5 @@ func main()  {
 	AIsContainedInB(230, 255)
 	AContainsB()
 	TheSetOfxInASuchThatP()
+	ThePowerSetOfA()
 }
